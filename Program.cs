6a4213c4e91bf9629ef25cbc9038626141243dda
IlyaBorisov <1.borisov.ilya@gmail.com>
2019-10-10 12:07:34 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog;
+using System;
+
+namespace LeadsBlack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Common.InitErrorHandlers();
+            if (Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == Environments.Development)
+                LogManager.LoadConfiguration("nlog.Development.config");
+            else
+                LogManager.LoadConfiguration("nlog.config");
+            Logger logger = LogManager.GetCurrentClassLogger();
+            LogManager.ThrowExceptions = true;
+            LogManager.ThrowConfigExceptions = true;
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, ex.ToString());
+                throw;
+            }
+            finally
+            {
+                LogManager.Flush();
+                LogManager.Shutdown();
+            }
+        }
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseSystemd()
+                .ConfigureLogging(logging => logging.ClearProviders().AddNLog(new NLogProviderOptions
+                {
+                    CaptureMessageTemplates = true,
+                    CaptureMessageProperties = true
+                }))
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services
+                        .AddSingleton<LeadsHandler>()
+                        .AddSingleton<OrdersHandler>()
+                        .AddHostedService<DatabaseObserver<LeadsHandler>>()
+                        .AddHostedService<DatabaseObserver<OrdersHandler>>();
+                });
+        }
+    }
+}
